a18fb15c4ad59f1d9e2fef3fec2e299740ed8c36
Robert Shaw <r.shaw1@yahoo.com>
2017-08-17 14:29:26 -0500

    completed section 3

--- models/day.js
+++ models/day.js
@@ -6,4 +6,30 @@ var Day = db.define('day', {
   number: Sequelize.INTEGER
 });
 
+Day.hook('afterBulkDestroy', (queryObj) => {
+  //console.log("day object",day)
+  var deletedDayNum = queryObj.where.number
+
+  //find each element's number and check if it is greater than deletedDayNum
+
+  //if its greater than deletedDayNum, subtract 1 from number
+  return Day.findAll({
+    where: {
+      number: {
+        $gt: deletedDayNum
+      }
+    }
+  })
+    .then((daysToUpdate) => {
+      var updatedDays = daysToUpdate.map(day => {
+        day.number = day.number - 1;
+        return day.save()
+      })
+
+      return Promise.all(updatedDays)
+
+    })
+}) ;
+
+
 module.exports = Day;
